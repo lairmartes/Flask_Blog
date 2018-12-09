8b5ccea9bbde2a647df2d3214b5510be8e85851d
Reed Richards <reed@debian-first-reed.debian-first.reed.net>
2018-12-08 19:44:44 -0600

    Flask tutorial from Corey Schafer

---
+++ flaskblog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Post 1',
+		'content': 'First post contet',
+		'date_posted': 'April 20th, 2018'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post contet',
+		'date_posted': 'April 21st, 2018'
+	}
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+	return render_template('about.html', title='About')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
